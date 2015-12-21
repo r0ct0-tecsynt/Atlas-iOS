b02a106350903e4248a59679e458d0b14ca3c070
p0ct0 <poctuclab@gmail.com>
2015-12-21 06:43:36 +0200

    test commit

--- Code/Controllers/ATLConversationListViewController.m
+++ Code/Controllers/ATLConversationListViewController.m
@@ -84,7 +84,7 @@ NSString *const ATLConversationListViewControllerDeletionModeGlobal = @"Global";
     _deletionModes = @[@(LYRDeletionModeLocal), @(LYRDeletionModeAllParticipants)];
     _displaysAvatarItem = NO;
     _allowsEditing = YES;
-    _rowHeight = 76.0f;
+    _rowHeight = 46.0f;
     _shouldDisplaySearchController = YES;
 }
 
